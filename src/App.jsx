70752974fc2b4aa5bf960e232ae59e95aa979e79
Daniel Co <dcohomemail@gmail.com>
2021-01-16 00:58:40 -0800

    consolidate all button functions into one

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,19 @@ import axios from 'axios';
 import React from 'react';
 
 const App = () => {
-    const startInterval = () => {
-        axios.get('/startInterval');
-    };
-
-    const stopInterval = () => {
-        axios.get('/stopInterval');
-    };
-
-    const runBPS = () => {
-        axios.get('/runBPS');
-    };
-
-    const runHWS = () => {
-        axios.get('/runHWS');
-    };
-
-    const clearBPSChannel = () => {
-        axios.get('/clearBPSChannel');
-    };
-
-    const clearHWSChannel = () => {
-        axios.get('/clearHWSChannel');
-    };
+    const handleClick = (path) => {
+        axios.get(`/${path}`);
+    }
 
     return (
-        <div>
-            <button onClick={startInterval}>Start Interval</button>
-            <button onClick={stopInterval}>Stop Interval</button>
-            <button onClick={runBPS}>Run BPS</button>
-            <button onClick={runHWS}>Run HWS</button>
-            <button onClick={clearBPSChannel}>Clear Messages in BPS Channel</button>
-            <button onClick={clearHWSChannel}>Clear Messages in HWS Channel</button>
+        <div onClick={e => handleClick(e.target.value)}>
+            <button value={"startBPSInterval"}>Start BPS Interval</button>
+            <button value={"startHWSInterval"}>Start HWS Interval</button>
+            <button value={"stopInterval"}>Stop Interval</button>
+            <button value={"runBPS"}>Run BPS</button>
+            <button value={"runHWS"}>Run HWS</button>
+            <button value={"clearBPSChannel"}>Clear Messages in BPS Channel</button>
+            <button value={"clearHWSChannel"}>Clear Messages in HWS Channel</button>
         </div>
     )
 };
