8ea6c347f2c084d80e973d66c1c4b0f4107b2316
Daniel Co <dcohomemail@gmail.com>
2021-01-16 00:56:56 -0800

    move message functions to separate file

---
+++ src/helpers/message.js
@@ -0,0 +1,53 @@
+const clients = require("../helpers/clients");
+
+const bpsChannelID = "794367501663600672";
+const hwsChannelID = "795863235215360001";
+
+const sendMessages = (posts, subreddit) => {
+    let channel = null;
+
+    switch (subreddit) {
+        case "bps":
+            channel = clients.discord.channels.cache.get(bpsChannelID);
+            posts.forEach((post) =>
+                channel.send(
+                    post.title +
+                        "\n\nProduct Link: " +
+                        post.url +
+                        "\n\nSee Comments: https://www.reddit.com/" +
+                        post.permalink
+                )
+            );
+            break;
+        case "hws":
+            channel = clients.discord.channels.cache.get(hwsChannelID);
+            posts.forEach((post) =>
+                channel.send(
+                    post.title +
+                        "\n\nSee Comments: https://www.reddit.com/" +
+                        post.permalink
+                )
+            );
+            break;
+    }
+};
+
+const deleteMessages = (channel) => {
+    let channelObj = null;
+
+    switch (channel) {
+        case "bps":
+            channelObj = clients.discord.channels.cache.get(bpsChannelID);
+            channelObj.bulkDelete(100);
+            console.log("BPS Channel Cleared");
+            break;
+        case "hws":
+            channelObj = clients.discord.channels.cache.get(hwsChannelID);
+            channelObj.bulkDelete(100);
+            console.log("HWS Channel Cleared");
+            break;
+    }
+};
+
+exports.send = sendMessages;
+exports.delete = deleteMessages;
