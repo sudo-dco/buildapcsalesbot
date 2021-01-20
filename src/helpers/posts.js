7107d92e78f944cc7f237d65c288a711e2c679b3
Daniel Co <dcohomemail@gmail.com>
2021-01-20 00:08:29 -0800

    add code to write error message to logs

--- src/helpers/posts.js
+++ src/helpers/posts.js
@@ -1,16 +1,24 @@
-const clients = require("../helpers/clients");
+const clients = require("./clients");
+const fs = require("./fs");
 
-const fetchNewPosts = (subreddit) => {
-    return clients.snoowrap
-        .getSubreddit(subreddit)
-        .getNew()
-        .map((post) => ({
-            title: post.title,
-            id: post.id,
-            url: post.url,
-            created: post.created,
-            permalink: post.permalink,
-        }));
+const fetchNewPosts = async (subreddit) => {
+    try {
+        const posts = await clients.snoowrap
+            .getSubreddit(subreddit)
+            .getNew()
+            .map((post) => ({
+                title: post.title,
+                id: post.id,
+                url: post.url,
+                created: post.created,
+                permalink: post.permalink,
+            }));
+
+        return posts;
+    } catch (error) {
+        console.log("Error fetching posts");
+        fs.setErrorLog(error.toString());
+    }
 };
 
 exports.fetchNew = fetchNewPosts;
