51c570629257adb860adaa881dfa9ea9ec887582
Daniel Co <dcohomemail@gmail.com>
2021-01-22 18:31:43 -0800

    add tests for refactored get and set timestamp functions

--- tests/fs.test.js
+++ tests/fs.test.js
@@ -1,16 +1,6 @@
 require("regenerator-runtime/runtime");
 const fs = require("../src/helpers/fs");
 
-xtest("write times to timestamp file correctly", async () => {
-    const fn = await checkLU.setTimestamp("1234567890");
-    expect(fn).toBe(0);
-});
-
-xtest("read times from timestamp file correctly", async () => {
-    const fn = await checkLU.getTimestamp();
-    expect(fn).toBe("1234567890");
-});
-
 test("write to bps error log with status code 0", async () => {
     const status = await fs.setErrorLog("test", "bps");
     expect(status).toBe(0);
@@ -19,4 +9,20 @@ test("write to bps error log with status code 0", async () => {
 test("write to hws error log with status code 0", async () => {
     const status = await fs.setErrorLog("test", "hws");
     expect(status).toBe(0);
+});
+
+test("write to and read from bps timestamp log", async () => {
+    const status = await fs.setTimestamp("test", "bps");
+    if (status === 0) {
+        const data = await fs.getTimestamp("bps");
+        expect(data).toBe("test");
+    }
+});
+
+test("write to and read from hws timestamp log", async () => {
+    const status = await fs.setTimestamp("test", "hws");
+    if (status === 0) {
+        const data = await fs.getTimestamp("hws");
+        expect(data).toBe("test");
+    }
 });
